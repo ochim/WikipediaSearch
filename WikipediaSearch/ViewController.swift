ab50355d43bf8c0fb3b9c5631604a6daa2ae47d4
ochi <mune.ochi@gmail.com>
2019-01-31 20:49:11 +0900

    UI作成

--- WikipediaSearch/ViewController.swift
+++ WikipediaSearch/ViewController.swift
@@ -7,14 +7,19 @@
 //
 
 import UIKit
+import RxSwift
+import RxCocoa
 
 class ViewController: UIViewController {
+
+    @IBOutlet weak var searchBar: UISearchBar!
+    @IBOutlet weak var tableView: UITableView!
+    let disposeBag = DisposeBag()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
-
+    
 }
 
