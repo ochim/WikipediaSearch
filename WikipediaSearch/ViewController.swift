0847ce123243817accf02538bddb50cc9ffb7039
ochi <mune.ochi@gmail.com>
2019-01-31 19:11:52 +0900

    Initial Commit

---
+++ WikipediaSearch/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WikipediaSearch
+//
+//  Created by nijibox088 on 2019/01/31.
+//  Copyright © 2019年 recruit. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
