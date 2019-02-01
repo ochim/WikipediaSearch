bf81083cf820e8a7a22326e611c024b3378775a5
ochi <mune.ochi@gmail.com>
2019-02-01 16:21:41 +0900

    キーワード検索とその結果表示

--- WikipediaSearch/ViewController.swift
+++ WikipediaSearch/ViewController.swift
@@ -19,7 +19,40 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        self.searchBar.rx.text.orEmpty
+            .filter { $0.count >= 3 }
+            .map { let urlStr = "https://ja.wikipedia.org/w/api.php?format=json&action=query&list=search&srsearch=\($0)"
+                return URL(string: urlStr.addingPercentEncoding(withAllowedCharacters: NSCharacterSet.urlQueryAllowed)!)!
+            }
+            .flatMapLatest { URLSession.shared.rx.json(url: $0) }
+            .map { self.parseJson($0) }
+            .bind(to: tableView.rx.items(cellIdentifier: "Cell")) { index, result, cell in
+                cell.textLabel?.text  = result.title
+                cell.detailTextLabel?.text = "https://ja.wikipedia.org/w/index.php?curid=\(result.pageid)"
+            }
+            .disposed(by: disposeBag)
     }
-    
+
+    func parseJson(_ json: Any) -> [Result] {
+        guard let items = json as? [String:Any] else {
+            return []
+        }
+        
+        var results = [Result]()
+        if let queryItems = items["query"] as? [String:Any] {
+            if let searchItems = queryItems["search"] as? [[String:Any]] {
+                searchItems.forEach {
+                    guard let title = $0["title"] as? String,
+                        let pageid = $0["pageid"] as? Int else { return }
+                    results.append(Result(title: title, pageid: pageid))
+                }
+            }
+        }
+        return results
+    }
 }
 
+struct Result {
+    let title: String
+    let pageid: Int
+}
